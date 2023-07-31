87e52e98e0fee5238347bcc62d62a8ed4ad7dad8
Shinaayomi Bamgboye~ <shinaayomibamgboye@gmail.com>
2023-08-01 00:58:28 +0100

    new

--- index.js
+++ index.js
@@ -1,33 +1,33 @@
-    // function generatePickupLine() {
-    //     const adjectives = ["charming", "magnetic", "dazzling", "enchanting", "captivating"];
-    //     const compliments = [
-    //     "You must be a shooting star because you light up my night.",
-    //     "Do you have a map? I keep getting lost in your eyes.",
-    //     "If you were a vegetable, you'd be a cute-cumber.",
-    //     "Is your name Google? Because you've got everything I've been searching for.",
-    //     "Your smile must be a black hole because it's impossible to escape its gravity.",
-    //     "Every moment with you feels like a dream I never want to wake up from.",
-    //     "I believe in destiny, and I think it led me to you.",
-    //     "You are the missing piece that completes the puzzle of my heart.",
-    //     "The world may be chaotic, but when I'm with you, everything feels just right.",
-    //     "Your presence is a melody that soothes my soul in the symphony of life."
-    //     ];
+    function generatePickupLine() {
+        const adjectives = ["charming", "magnetic", "dazzling", "enchanting", "captivating"];
+        const compliments = [
+        "You must be a shooting star because you light up my night.",
+        "Do you have a map? I keep getting lost in your eyes.",
+        "If you were a vegetable, you'd be a cute-cumber.",
+        "Is your name Google? Because you've got everything I've been searching for.",
+        "Your smile must be a black hole because it's impossible to escape its gravity.",
+        "Every moment with you feels like a dream I never want to wake up from.",
+        "I believe in destiny, and I think it led me to you.",
+        "You are the missing piece that completes the puzzle of my heart.",
+        "The world may be chaotic, but when I'm with you, everything feels just right.",
+        "Your presence is a melody that soothes my soul in the symphony of life."
+        ];
     
-    //     const randomAdjective = adjectives[Math.floor(Math.random() * adjectives.length)];
-    //     const randomCompliment = compliments[Math.floor(Math.random() * compliments.length)];
+        const randomAdjective = adjectives[Math.floor(Math.random() * adjectives.length)];
+        const randomCompliment = compliments[Math.floor(Math.random() * compliments.length)];
     
-    //     return `  ${randomCompliment}`;
-    // }
+        return `  ${randomCompliment}`;
+    }
     
-    // document.addEventListener("DOMContentLoaded", function() {
-    //     const generateButton = document.getElementById("generateButton");
-    //     const pickupLineElement = document.getElementById("pickupLine");
+    document.addEventListener("DOMContentLoaded", function() {
+        const generateButton = document.getElementById("generateButton");
+        const pickupLineElement = document.getElementById("pickupLine");
     
-    //     generateButton.addEventListener("click", function() {
-    //     const pickupLine = generatePickupLine();
-    //     pickupLineElement.textContent = pickupLine;
-    //     });
-    // });
+        generateButton.addEventListener("click", function() {
+        const pickupLine = generatePickupLine();
+        pickupLineElement.textContent = pickupLine;
+        });
+    });
 
   //second one
 
@@ -105,38 +105,38 @@
   
 // script.js
 // script.js
-document.addEventListener("DOMContentLoaded", function () {
-    const generateButton = document.getElementById("generateButton");
-    const pickupLineElement = document.getElementById("pickupLine");
+// document.addEventListener("DOMContentLoaded", function () {
+//     const generateButton = document.getElementById("generateButton");
+//     const pickupLineElement = document.getElementById("pickupLine");
   
-    generateButton.addEventListener("click", function () {
-      // Call the function to generate a pickup line using the AI
-      generatePickupLine().then((pickupLine) => {
-        pickupLineElement.textContent = pickupLine;
-      });
-    });
-  });
+//     generateButton.addEventListener("click", function () {
+//       // Call the function to generate a pickup line using the AI
+//       generatePickupLine().then((pickupLine) => {
+//         pickupLineElement.textContent = pickupLine;
+//       });
+//     });
+//   });
   
-  async function generatePickupLine() {
-    // Replace 'YOUR_GPT3_API_KEY' with your actual GPT-3 API key
-    const apiKey = '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';
-    const prompt = 'Generate a pickup line for me.';
-    const url = 'https://api.openai.com/v1/engines/text-davinci-002/completions';
+//   async function generatePickupLine() {
+//     // Replace 'YOUR_GPT3_API_KEY' with your actual GPT-3 API key
+//     const apiKey = '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';
+//     const prompt = 'Generate a pickup line for me.';
+//     const url = 'https://api.openai.com/v1/engines/text-davinci-002/completions';
   
-    const response = await fetch(url, {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json',
-        'Authorization': `Bearer ${apiKey}`
-      },
-      body: JSON.stringify({
-        'prompt': prompt,
-        'max_tokens': 50, // Adjust the number of tokens to control the response length
-        'temperature': 0.7 // Adjust the temperature for more or less randomness (0.2 to 1.0)
-      })
-    });
+//     const response = await fetch(url, {
+//       method: 'POST',
+//       headers: {
+//         'Content-Type': 'application/json',
+//         'Authorization': `Bearer ${apiKey}`
+//       },
+//       body: JSON.stringify({
+//         'prompt': prompt,
+//         'max_tokens': 50, // Adjust the number of tokens to control the response length
+//         'temperature': 0.7 // Adjust the temperature for more or less randomness (0.2 to 1.0)
+//       })
+//     });
   
-    const data = await response.json();
-    return data.choices[0].text.trim();
-  }
+//     const data = await response.json();
+//     return data.choices[0].text.trim();
+//   }
   
